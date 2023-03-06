68c1bd41b4ac67768da1d8182451f9857d4e582b
Logan3106 <529295@chrome.escg.ac.uk>
2023-03-06 13:55:59 +0000

    Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 6f;
+    public float rotationspeed = 6f;
 
     public Rigidbody rb;
 
-    Vector3 movement;
+    
 
     public void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        movement.Normalize();
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion torotation = Quaternion.LookRotation(movement, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, torotation, rotationspeed * Time.deltaTime);
+        }
     }
 
-    public void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-    }
 
 }
